0c0d868f138ecebd85ffc8bc0730fa5b0a16c991
Ryan Guill <ryanguill@gmail.com>
2020-03-06 21:46:22 -0600

    initial commit

---
+++ build/index.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $("label.toggle-on-click").on("click", function(e) {
+    const $target = $(e.target).find("select");
+    if (!$target.length) {
+      return;
+    }
+    const selectedIndex = $target.get(0).selectedIndex;
+    const options = [...$target.get(0).options];
+    let newSelectedIndex = selectedIndex + 1;
+    if (newSelectedIndex > options.length - 1) {
+      newSelectedIndex = 0;
+    }
+    $target.prop("selectedIndex", newSelectedIndex);
+    render();
+  });
+
+  $("#input").on("change keyup", render);
+
+  render();
+});
+
+function render() {
+  const settings = {
+    flatten_indentation: $("#flatten-indentation").val() === "true",
+    remove_bullets: $("#remove-bullets").val() === "true",
+    remove_double_brackets: $("#remove-double-brackets").val() === "true"
+  };
+
+  //console.log(settings);
+
+  const input = $("#input").val();
+  let result = input;
+
+  if (settings.flatten_indentation) {
+    result = flattenIndentation(result);
+  }
+
+  if (settings.remove_bullets) {
+    result = removeBullets(result);
+  }
+
+  if (settings.remove_double_brackets) {
+    result = removeDoubleBrackets(result);
+  }
+
+  $("#output").val(result);
+}
+
+function flattenIndentation(input) {
+  return input
+    .split("\n")
+    .map(function(line) {
+      return line.trimStart();
+    })
+    .join("\n");
+}
+
+function removeBullets(input) {
+  return input
+    .split("\n")
+    .map(function(line) {
+      return line.replace(/^(\s*)-\s/gm, "$1");
+    })
+    .join("\n");
+}
+
+function removeDoubleBrackets(input) {
+  return input
+    .split("\n")
+    .map(function(line) {
+      return line
+        .replace(/\[([^\[\]]+)\]\((\[\[|\(\()([^\[\]]+)(\]\]|\)\))\)/gm, "$1")
+        .replace(/\[\[([^\[\]]+)\]\]/gm, "$1")
+        .replace(/\(\(([^\(\)]+)\)\)/gm, "$1");
+    })
+    .join("\n");
+}
